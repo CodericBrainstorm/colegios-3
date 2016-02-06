2b8c620a743d6116082029e358d8f6fd38e744fe
pablonm <pablomiceli24@gmail.com>
2016-02-06 02:56:24 -0300

    Listado de usuarios para el admin
    Avances en las vistas para los listados de usuarios sostenedores,
    directores y miembros para el administrador.

--- src/AppBundle/Controller/adminController.php
+++ src/AppBundle/Controller/adminController.php
@@ -20,5 +20,42 @@ class adminController extends Controller
             array('params' => 1)
         );
     }
+    
+    /**
+    * @Route("/admin/sostenedores/", name="sostenedores")
+    */
+    
+    public function sostenedoresAction()
+    {
+        return $this->render(
+            'admin/sostenedores.html.twig',
+            array('params' => 1)
+        );
+    }
+    
+    /**
+    * @Route("/admin/directores/", name="directores")
+    */
+    
+    public function directoresAction()
+    {
+        return $this->render(
+            'admin/directores.html.twig',
+            array('params' => 1)
+        );
+    }
+    
+    /**
+    * @Route("/admin/miembros/", name="miembros")
+    */
+    
+    public function miembrosAction()
+    {
+        return $this->render(
+            'admin/miembros.html.twig',
+            array('params' => 1)
+        );
+    }
+    
 
 }
