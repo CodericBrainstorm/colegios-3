3d38082c3e81033ad030e74cb49038eacd89234f
pablonm <pablomiceli24@gmail.com>
2016-02-08 19:29:41 -0300

    Terminadas las vistas principales del admin
    A grandes rasgos..

--- src/AppBundle/Controller/adminController.php
+++ src/AppBundle/Controller/adminController.php
@@ -46,6 +46,50 @@ class adminController extends Controller
     }
     
     /**
+    * @Route("/admin/editar_sostenedor/{id}", name="editar sostenedor")
+    */
+    
+    public function editarSostenedorAction($id)
+    {
+        return $this->render(
+            'admin/form_sostenedor.html.twig',
+            array(
+                'op' => "modificacion",
+                'nombre' => "asd",
+                'tipo_de_institucion' => "asd",
+                'email' => "asd",
+                'usuario' => "asd",
+                'contraseña' => "asd",
+                'ciudad' => "asd",
+                'comuna' => "asd",
+                'direccion' => "asd"
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_sostenedor/{id}", name="ver sostenedor")
+    */
+    
+    public function verSostenedorAction($id)
+    {
+        return $this->render(
+            'admin/form_sostenedor.html.twig',
+            array(
+                'op' => "vista",
+                'nombre' => "asd",
+                'tipo_de_institucion' => "asd",
+                'email' => "asd",
+                'usuario' => "asd",
+                'contraseña' => "asd",
+                'ciudad' => "asd",
+                'comuna' => "asd",
+                'direccion' => "asd"
+                )
+        );
+    }
+    
+    /**
     * @Route("/admin/directores/", name="directores")
     */
     
@@ -66,6 +110,46 @@ class adminController extends Controller
         return $this->render(
             'admin/form_director.html.twig',
             array('op' => "alta")
+        );
+    }
+    
+    /**
+    * @Route("/admin/editar_director/{id}", name="editar director")
+    */
+    
+    public function editarDirectorAction($id)
+    {
+        return $this->render(
+            'admin/form_director.html.twig',
+            array(
+                'op' => "modificacion",
+                'sostenedor' => "asd",
+                'nombre' => "asd",
+                'tipo_de_escuela' => "asd",
+                'escuela' => "asd",
+                'usuario' => "asd",
+                'contraseña' => "asd"
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_director/{id}", name="ver director")
+    */
+    
+    public function verDirectorAction($id)
+    {
+        return $this->render(
+            'admin/form_director.html.twig',
+            array(
+                'op' => "vista",
+                'sostenedor' => "asd",
+                'nombre' => "asd",
+                'tipo_de_escuela' => "asd",
+                'escuela' => "asd",
+                'usuario' => "asd",
+                'contraseña' => "asd"
+                )
         );
     }
     
@@ -94,6 +178,42 @@ class adminController extends Controller
     }
     
     /**
+    * @Route("/admin/editar_miembro/{id}", name="editar miembro")
+    */
+    
+    public function editarMiembroAction($id)
+    {
+        return $this->render(
+            'admin/form_miembro.html.twig',
+            array(
+                'op' => "modificacion",
+                'director' => "asd",
+                'nombre' => "asd",
+                'usuario' => "asd",
+                'contraseña' => "asd"
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_miembro/{id}", name="ver miembro")
+    */
+    
+    public function verMiembroAction($id)
+    {
+        return $this->render(
+            'admin/form_miembro.html.twig',
+            array(
+                'op' => "vista",
+                'director' => "asd",
+                'nombre' => "asd",
+                'usuario' => "asd",
+                'contraseña' => "asd"
+                )
+        );
+    }
+    
+    /**
     * @Route("/admin/rubricas/", name="rubricas")
     */
     
@@ -114,6 +234,36 @@ class adminController extends Controller
         return $this->render(
             'admin/form_rubrica.html.twig',
             array('op' => "alta")
+        );
+    }
+    
+    /**
+    * @Route("/admin/editar_rubrica/", name="editar rubrica")
+    */
+    
+    public function editarRubricaAction()
+    {
+        return $this->render(
+            'admin/form_rubrica.html.twig',
+            array(
+                'op' => "modificacion", 
+                'nombre' => "asd" 
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_rubrica/", name="ver rubrica")
+    */
+    
+    public function verRubricaAction()
+    {
+        return $this->render(
+            'admin/form_rubrica.html.twig',
+            array(
+                'op' => "vista", 
+                'nombre' => "asd" 
+                )
         );
     }
     
@@ -142,6 +292,36 @@ class adminController extends Controller
     }
     
     /**
+    * @Route("/admin/editar_tipo_institucion/", name="editar tipo de institucion")
+    */
+    
+    public function editarTipoInstitucionAction()
+    {
+        return $this->render(
+            'admin/form_tipo_institucion.html.twig',
+            array(
+                'op' => "modificacion", 
+                'nombre' => "asd" 
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_tipo_institucion/", name="ver tipo de institucion")
+    */
+    
+    public function verTipoInstitucionAction()
+    {
+        return $this->render(
+            'admin/form_tipo_institucion.html.twig',
+            array(
+                'op' => "vista", 
+                'nombre' => "asd" 
+                )
+        );
+    }
+    
+    /**
     * @Route("/admin/ciudades/", name="ciudades")
     */
     
@@ -162,6 +342,36 @@ class adminController extends Controller
         return $this->render(
             'admin/form_ciudad.html.twig',
             array('op' => "alta")
+        );
+    }
+    
+    /**
+    * @Route("/admin/editar_ciudad/", name="editar ciudad")
+    */
+    
+    public function editarCiudadAction()
+    {
+        return $this->render(
+            'admin/form_ciudad.html.twig',
+            array(
+                'op' => "modificacion", 
+                'nombre' => "asd" 
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_ciudad/", name="ver ciudad")
+    */
+    
+    public function verCiudadAction()
+    {
+        return $this->render(
+            'admin/form_ciudad.html.twig',
+            array(
+                'op' => "vista", 
+                'nombre' => "asd" 
+                )
         );
     }
     
@@ -190,6 +400,94 @@ class adminController extends Controller
     }
     
     /**
+    * @Route("/admin/editar_comuna/", name="editar comuna")
+    */
+    
+    public function editarComunaAction()
+    {
+        return $this->render(
+            'admin/form_comuna.html.twig',
+            array(
+                'op' => "modificacion", 
+                'nombre' => "asd", 
+                'ciudad' => "asd" 
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_comuna/", name="ver comuna")
+    */
+    
+    public function verComunaAction()
+    {
+        return $this->render(
+            'admin/form_comuna.html.twig',
+            array(
+                'op' => "vista", 
+                'nombre' => "asd", 
+                'ciudad' => "asd" 
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/escuelas/", name="escuelas")
+    */
+    
+    public function escuelasAction()
+    {
+        return $this->render(
+            'admin/escuelas.html.twig',
+            array('params' => 1)
+        );
+    }
+    
+    /**
+    * @Route("/admin/nueva_escuela/", name="nueva escuela")
+    */
+    
+    public function nuevaEscuelaAction()
+    {
+        return $this->render(
+            'admin/form_escuela.html.twig',
+            array('op' => "alta")
+        );
+    }
+    
+    /**
+    * @Route("/admin/editar_escuela/", name="editar escuela")
+    */
+    
+    public function editarEscuelaAction()
+    {
+        return $this->render(
+            'admin/form_escuela.html.twig',
+            array(
+                'op' => "modificacion", 
+                'nombre' => "asd", 
+                'comuna' => "asd" 
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_escuela/", name="ver escuela")
+    */
+    
+    public function verEscuelaAction()
+    {
+        return $this->render(
+            'admin/form_escuela.html.twig',
+            array(
+                'op' => "vista", 
+                'nombre' => "asd", 
+                'comuna' => "asd" 
+                )
+        );
+    }
+    
+    /**
     * @Route("/admin/areas/", name="areas")
     */
     
@@ -210,6 +508,36 @@ class adminController extends Controller
         return $this->render(
             'admin/form_area.html.twig',
             array('op' => "alta")
+        );
+    }
+    
+    /**
+    * @Route("/admin/editar_area/", name="editar area")
+    */
+    
+    public function editarAreaAction()
+    {
+        return $this->render(
+            'admin/form_area.html.twig',
+            array(
+                'op' => "modificacion", 
+                'nombre' => "asd" 
+                )
+        );
+    }
+    
+    /**
+    * @Route("/admin/ver_area/", name="ver area")
+    */
+    
+    public function verAreaAction()
+    {
+        return $this->render(
+            'admin/form_area.html.twig',
+            array(
+                'op' => "vista", 
+                'nombre' => "asd" 
+                )
         );
     }
     
