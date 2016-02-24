086b04fbd152cfaef9429b2015edd1481a071d0f
pablonm <pablomiceli24@gmail.com>
2016-02-24 18:38:11 -0300

    merge

--- src/AppBundle/Form/Type/AdministradorType.php
+++ src/AppBundle/Form/Type/AdministradorType.php
@@ -2,13 +2,17 @@
 
 namespace AppBundle\Form\Type;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class AdministradorType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        
+        $builder->add('ano', EntityType::class, array(
+            'class' => 'AppBundle:Ano',
+            'choice_label' => 'nombre'
+        ));
     }
 
     public function getParent() {
